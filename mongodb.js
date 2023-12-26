286033a40f846cbd37b1a29216c5bcb9f2760fb2
gurwindaniel <gurwindaniel@gmail.com>
2023-12-26 14:12:52 +0530

    add git ignore to modules

---
+++ mongodb.js
@@ -0,0 +1,42 @@
+// CRUD Create Read Update and Delete
+const {MongoClient,ServerApiVersion}=require('mongodb')
+
+
+
+const connectionURL='mongodb://127.0.0.1:27017'
+const databaseName='task-manager'
+
+
+const client= new MongoClient(connectionURL,{
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run(){
+    try{
+
+        await client.connect()
+        
+        console.log("Connected to MongoDB")
+         const db=await client.db("task-manager")
+        db.collection('users').insertOne({
+            name:'Daniel3',
+            email:'daniel3@gmail.com',
+            age:27
+        })
+
+    }catch(e){
+        console.log("Error occured")
+        console.log(e)
+    }finally{
+        // client.close()
+    }
+}
+
+
+run();
+
+
